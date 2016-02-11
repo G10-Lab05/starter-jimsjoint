63ab9aaf13662738c547919564e5a6212843e1fa
josephwithay <yoseph_jo@hotmail.com>
2016-02-11 13:58:01 -0800

    changelog updated and step 3 is fixed
    Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,36 +11,34 @@
  */
 class Welcome extends Application {
 
-    function __construct() {
-        parent::__construct();
-    }
+	function __construct() {
+	parent::__construct();
+	}
 
-    //-------------------------------------------------------------
-    //  The normal pages
-    //-------------------------------------------------------------
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
 
-    function index() {
-        $this->data['title'] = 'Jim\'s Joint!';
-        $this->data['pagebody'] = 'welcome';
+	function index() {
+		$this->data['title'] = 'Jim\'s Joint!';
+		$this->data['pagebody'] = 'welcome';
 
-        // Get all the completed orders
-        //FIXME
+		// Get all the completed orders
+		//FIXME
 
-        // Build a multi-dimensional array for reporting
-        $orders = array();
-        foreach ($completed as $order) {
-            $this1 = array(
-                'num' => $order->num,
-                'datetime' => $order->date,
-                'amount' => $order->total
-            );
-            $orders[] = $this1;
-        }
+		// Build a multi-dimensional array for reporting
+		$orders = array();
+		foreach ($completed as $order) {
+			$this1 = array(
+				'num' => $order->num,
+				'datetime' => $order->date,
+				'amount' => $order->total
+			);
+			$orders[] = $this1;
+		}
 
-        // and pass these on to the view
-        $this->data['orders'] = $orders;
-        
-        $this->render();
-    }
-
+		// and pass these on to the view
+		$this->data['orders'] = $orders;
+			$this->render();
+	}
 }
